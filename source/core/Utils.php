d80107a885a659475bf9966fb33261b5a495c2aa
HULK\Admin <sena@iocode.com.br>
2019-02-26 21:07:37 -0300

    Add CNPJ checker

--- source/core/Utils.php
+++ source/core/Utils.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Sounoob\pagseguro\core;
 
 /**
@@ -7,51 +8,42 @@ namespace Sounoob\pagseguro\core;
  */
 class Utils
 {
+
     /**
-     * @param $number
-     * @return string|string[]|null
+     * @param int $number
+     * @return int
      */
     static public function onlyNumbers($number)
     {
-        return preg_replace('/[^0-9]/', '', $number);
+        return (int)preg_replace('/[^0-9]/', '', $number);
     }
 
     /**
-     * @param $email
-     * @return bool|mixed
+     * @param string $email
+     * @return string|bool
      */
     static public function getDomainFromEmail($email)
     {
         $domain = explode('@', $email);
-        
-        if(count($domain) != 2) {
+
+        if (count($domain) != 2) {
             return false;
         }
         return end($domain);
     }
 
     /**
-     * @param null $cpf
+     * @param int $cpf
      * @return bool
      */
-    static public function checkCPF($cpf = null)
+    static public function checkCPF($cpf)
     {
         if (empty($cpf)) {
             return false;
         }
         $cpf = self::onlyNumbers($cpf);
         $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
-        if (strlen($cpf) != 11 ||
-            $cpf == '00000000000' ||
-            $cpf == '11111111111' ||
-            $cpf == '22222222222' ||
-            $cpf == '33333333333' ||
-            $cpf == '44444444444' ||
-            $cpf == '55555555555' ||
-            $cpf == '66666666666' ||
-            $cpf == '77777777777' ||
-            $cpf == '88888888888' ||
-            $cpf == '99999999999') {
+        if (strlen($cpf) != 11 || preg_match('/(\d)\1{10}/', $cpf)) {
             return false;
         }
         for ($verifier = 9; $verifier < 11; $verifier++) {
@@ -65,4 +57,36 @@ class Utils
         }
         return true;
     }
+
+    /**
+     * @param int $cnpj
+     * @return bool
+     */
+    static public function checkCNPJ($cnpj)
+    {
+        if (empty($cnpj)) {
+            return false;
+        }
+        $cnpj = self::onlyNumbers($cnpj);
+        $cnpj = str_pad($cnpj, 14, '0', STR_PAD_LEFT);
+
+        if (strlen($cnpj) != 14 || preg_match('/(\d)\1{13}/', $cnpj)) {
+            return false;
+        }
+
+        for ($i = 0, $j = 5, $sum = 0; $i < 12; $i++) {
+            $sum += $cnpj{$i} * $j;
+            $j = ($j == 2) ? 9 : $j - 1;
+        }
+        $verifier = $sum % 11;
+        if ($cnpj{12} != ($verifier < 2 ? 0 : 11 - $verifier))
+            return false;
+        for ($i = 0, $j = 6, $sum = 0; $i < 13; $i++) {
+            $sum += $cnpj{$i} * $j;
+            $j = ($j == 2) ? 9 : $j - 1;
+        }
+        $verifier = $sum % 11;
+        return $cnpj{13} == ($verifier < 2 ? 0 : 11 - $verifier);
+    }
+
 }
