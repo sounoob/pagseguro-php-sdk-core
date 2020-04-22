5f6f3bc121f528cb2b4789452649471fc8bb35c7
Flavio Sena <flavio.sena@i9xp.com.br>
2020-04-22 19:38:30 -0300

    Add cnpj and email from domain test

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -16,4 +16,20 @@ class UtilsTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(false, \Sounoob\pagseguro\core\Utils::checkCPF('000.000.000-00'));
         $this->assertEquals(false, \Sounoob\pagseguro\core\Utils::checkCPF('00000000000'));
     }
+
+    public function testCNPJ()
+    {
+        $this->assertEquals(true, \Sounoob\pagseguro\core\Utils::checkCNPJ('02.287.830/0001-77'));
+        $this->assertEquals(true, \Sounoob\pagseguro\core\Utils::checkCNPJ('25936072000176'));
+        $this->assertEquals(false, \Sounoob\pagseguro\core\Utils::checkCNPJ('02.288.703/0001-77'));
+        $this->assertEquals(false, \Sounoob\pagseguro\core\Utils::checkCNPJ('00.000.000/0000-00'));
+        $this->assertEquals(false, \Sounoob\pagseguro\core\Utils::checkCNPJ('00000000000000'));
+    }
+
+    public function testEmailDomain()
+    {
+        $this->assertEquals('sounoob.com.br', \Sounoob\pagseguro\core\Utils::getDomainFromEmail('contato@sounoob.com.br'));
+        $this->assertEquals(false, \Sounoob\pagseguro\core\Utils::getDomainFromEmail('contato@sou@noob.com.br'));
+        $this->assertEquals(false, \Sounoob\pagseguro\core\Utils::getDomainFromEmail('sounoob.com.br'));
+    }
 }
